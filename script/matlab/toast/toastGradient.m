dc78d18951fc3a27c4006dbef9dbc4c6c48aab3d
Samuel Powell <samuelpowell@users.noreply.github.com>
2022-05-06 21:44:26 +0100

    Move script files, add to dist output

--- script/matlab/toast/toastGradient.m
+++ script/matlab/toast/toastGradient.m
@@ -1,23 +1,59 @@
-function p = toastGradient
-%toastGradient        - Return gradient of objective function.
+function p = toastGradient(hmesh,hbasis,qvec,mvec,mua,mus,ref,freq,data,sd,varargin)
+% toastGradient        - Return gradient of objective function.
 %
-% Synopsis: G = toastGradient(hMesh,hBasis,qvec,mvec,mua,mus,n,f,data,sd,solver,tol)
-%    hMesh:  mesh handle
-%    hBasis: basis handle
-%    qvec:   array of source column-vectors (complex sparse)
-%    mvec:   array of measurement column-vectors (complex sparse)
-%    mua:    nodal absorption values [1/mm] (real)
-%    mus:    nodal scattering values [1/mm] (real)
-%    n:      nodal refractive index values (real)
-%    f:      modulation frequency [MHz] (real scalar)
-%    data:   measurement data vector (real)
-%    sd:     measurement standard-deviation vector (real)
-%    solver: DIRECT|CG|BICG|BICGSTAB|GMRES|GAUSSSEIDEL (string)
-%    tol:    solver tolerance (optional, iterative solvers only)
-%    G:      gradient of objective function (real)
+% Syntax: g = toastGradient(mesh,basis,qvec,mvec,mua,mus,ref,freq,data,sd, ...)
+%
+% Parameters:
+%         mesh (toastMesh instance):
+%             mesh object
+%         basis (toastBasis instance):
+%             basis mapper object
+%         qvec (complex matrix n x nq):
+%             array of nodal source vectors
+%         mvec (complex matrix n x nm):
+%             array of nodal measurement vectors
+%         mua (real array n):
+%             array of nodal absorption coefficients [1/mm]
+%         mus (real array n):
+%             array of nodal scattering coefficients [1/mm]
+%         ref (real array n):
+%             array of nodal refractive indices
+%         freq (scalar):
+%             modulation frequency [MHz]
+%         data (real array nqm*2):
+%             array of measurement data (lnamp, phase)
+%         sd (real array nqm*2):
+%             array of standard deviations (lnamp, phase)
+%
+% Optional parameters:
+%         'Method', method (string):
+%             forward solver method
+%             (DIRECT|CG|BICG|BICGSTAB|GMRES|GAUSSSEIDEL)
+%         'Tolerance', tol (scalar):
+%             forward solver tolerance
+%         'Fields', phi (complex matrix n x nq):
+%             array of nodal fields for each source
+%         'Projections', proj (real array nqm*2):
+%             array of projection data (lnamp, phase)
+%         'Unwrap', true|false (boolean):
+%             flag for activating phase unwrapping
+%
+% Return values:
+%         g (real array slen*2):
+%             gradient of objective function (mua, mus) in inverse basis
 %
 % This function runs the forward solver with the nodal optical parameters
 % provided, and calculates the derivative of the objective function with
 % respect to the optical parameters.
 % The returned gradient vector is with respect to the inverse solution basis
-% defined by hBasis, rather than the nodal mesh basis.
+% defined by basis, rather than the nodal mesh basis.
+%
+% Note:  If the modulation frequency (freq) is set to 0, toastGradient
+%        computes the gradient for a steady-state problem. In that case,
+%        data, sd and proj (if provided) can be of length nqm and only
+%        contain the log amplitude component, omitting the phase component.
+%        (if a phase component is present, it is ignored).
+%        Likewise, qvec, mvec and phi (if provided) can be real-valued, as
+%        the imaginary component is ignored.
+
+p = toastmex(uint32(52),hmesh.handle,hbasis.handle,qvec,mvec,mua,mus,ref,freq,data,sd,varargin{:});
--- script/matlab/toast/toastRecon.m
+++ script/matlab/toast/toastRecon.m
@@ -4,12 +4,13 @@ function toastRecon(prm)
 % Synopsis: toastRecon(prm)
 %    prm: reconstruction parameter structure
 %
-% Runs a toast reconstruction with the parameters defined in prm.
-% prm contains information about measurements, meshes and grids,
-% tolerance limits for the forward and inverse solvers, regularisation
-% parameters, etc.
+% A high-level convenience function which runs a toast reconstruction with
+% the parameters defined in prm.
+% prm is a toastParam instance containing information about measurements,
+% meshes and grids, tolerance limits for the forward and inverse solvers,
+% regularisation parameters, etc.
 %
-% See also: toastReadParam
+% See also: toastParam
   
 disp('---------------------------------------')
 disp('Starting reconstruction')
@@ -17,8 +18,6 @@ disp('---------------------------------------')
 
 % ----------------------------------------------------------------------
 % Initialisations
-toastCatchErrors();
-
 global RES;  % a structure for collecting reconstruction results
 global LPRM; % a set of local parameters
 global scref;
@@ -35,27 +34,23 @@ c0 = 0.3;           % speed of light in vacuum [mm/ps]
 
 % ----------------------------------------------------------------------
 % Read a TOAST mesh definition from file.
-if isfield(prm,'fwdsolver') && isfield(prm.fwdsolver,'hmesh')
+if isfield(prm.fwdsolver,'hmesh')
     LPRM.hMesh = prm.fwdsolver.hmesh;
 else
-    LPRM.hMesh = toastReadMesh (prm.fwdsolver.meshfile);
-    toastReadQM (LPRM.hMesh, prm.meas.qmfile);
+    LPRM.hMesh = toastMesh (prm.fwdsolver.meshfile);
+    LPRM.hMesh.ReadQM (prm.meas.qmfile);
 end
-n = toastMeshNodeCount (LPRM.hMesh);
-dmask = toastDataLinkList (LPRM.hMesh);
+n = LPRM.hMesh.NodeCount ();
+dmask = LPRM.hMesh.DataLinkList ();
 
 % ----------------------------------------------------------------------
 % Set up the mapper between FEM and solution bases
-if isfield(prm,'solver') && isfield(prm.solver,'basis') && isfield(prm.solver.basis,'hbasis')
-    LPRM.hBasis = prm.solver.basis.hbasis;
-else
-    LPRM.hBasis = toastSetBasis ('LINEAR',LPRM.hMesh, prm.solver.basis.bdim, prm.solver.basis.bdim);
+if ~isfield(prm.solver.basis,'hbasis')
+    prm.solver.basis.hbasis = toastBasis(LPRM.hMesh,prm.solver.basis.bdim,'Linear');
 end
-RES.bdim = toastGetBasisSize(LPRM.hBasis);
+LPRM.hBasis = prm.solver.basis.hbasis;
+RES.bdim = LPRM.hBasis.Dims();
 blen = prod(RES.bdim);
-solmask = toastSolutionMask (LPRM.hBasis);
-solmask2 = [solmask solmask+blen];
-RES.solmask = solmask;
 
 % ----------------------------------------------------------------------
 % Set up homogeneous initial parameter estimates
@@ -74,13 +69,13 @@ if isfield(prm.data,'lnamp')
     mdata = prm.data.lnamp;
 else
     fprintf('Reading log amplitude data from %s\n', prm.data.lnampfile);
-    mdata = toastReadRealVector(prm.data.lnampfile);
+    mdata = toastReadVector(prm.data.lnampfile);
 end
 if isfield(prm.data,'phase')
     pdata = prm.data.phase;
 else
     fprintf('Reading phase data from %s\n', prm.data.phasefile);
-    pdata = toastReadRealVector(prm.data.phasefile);
+    pdata = toastReadVector(prm.data.phasefile);
 end
 LPRM.data = [mdata;pdata];
 m = length(LPRM.data);
@@ -88,12 +83,12 @@ fprintf('Data space dimension: %d\n', m);
 
 % ----------------------------------------------------------------------
 % Generate source vectors
-LPRM.qvec = toastQvec (LPRM.hMesh, prm.meas.src.type, prm.meas.src.prof, prm.meas.src.width);
+LPRM.qvec = LPRM.hMesh.Qvec (prm.meas.src.type, prm.meas.src.prof, prm.meas.src.width);
 fprintf ('Source vector set up: %d sources\n', size(LPRM.qvec,2));
 
 % ----------------------------------------------------------------------
 % Generate measurement vectors
-LPRM.mvec = toastMvec (LPRM.hMesh, prm.meas.det.prof, prm.meas.det.width);
+LPRM.mvec = LPRM.hMesh.Mvec (prm.meas.det.prof, prm.meas.det.width, ref);
 fprintf ('Detector vector setup: %d detectors\n', size(LPRM.mvec,2));
 
 % ----------------------------------------------------------------------
@@ -108,7 +103,7 @@ if isfield(prm.data,'useref') && prm.data.useref == true
     fprintf ('Using difference data\n');
     if isfield(prm.data.ref,'lnampfile')
         fprintf ('Log amplitude reference: %s\n', prm.data.ref.lnampfile);
-        mref = toastReadRealVector(prm.data.ref.lnampfile);
+        mref = toastReadVector(prm.data.ref.lnampfile);
         if length(mref) > 0
             mproj = proj(1:length(mdata));
             mdata = mdata-mref+mproj;
@@ -119,7 +114,7 @@ if isfield(prm.data,'useref') && prm.data.useref == true
     end
     if isfield(prm.data.ref,'phasefile')
         fprintf ('Phase reference: %s\n', prm.data.ref.phasefile);
-        pref = toastReadRealVector(prm.data.ref.phasefile);
+        pref = toastReadVector(prm.data.ref.phasefile);
         if length(pref) > 0
             pproj = proj(length(mdata)+1:end);
             pdata = pdata-pref+pproj;
@@ -148,17 +143,17 @@ LPRM.sd = [sd_lnmod;sd_phase];
 
 % ----------------------------------------------------------------------
 % initial parameter estimates in solution basis
-RES.bmua = toastMapMeshToBasis (LPRM.hBasis, mua);
-RES.bmus = toastMapMeshToBasis (LPRM.hBasis, mus);
+RES.bmua = LPRM.hBasis.Map ('M->B', mua);
+RES.bmus = LPRM.hBasis.Map ('M->B', mus);
 bmua_itr(1,:) = RES.bmua;
 bmus_itr(1,:) = RES.bmus;
 
-bcmua = toastMapMeshToBasis (LPRM.hBasis, mua .* (c0./ref));
-bckap = toastMapMeshToBasis (LPRM.hBasis, kap .* (c0./ref));
+bcmua = LPRM.hBasis.Map ('M->B', mua .* (c0./ref));
+bckap = LPRM.hBasis.Map ('M->B', kap .* (c0./ref));
 
-scmua = bcmua(solmask);
-sckap = bckap(solmask);
-scref = toastMapMeshToSol (LPRM.hBasis, c0./ref);
+scmua = LPRM.hBasis.Map ('B->S', bcmua);
+sckap = LPRM.hBasis.Map ('B->S', bckap);
+scref = LPRM.hBasis.Map ('M->S', c0./ref);
 
 x = [scmua;sckap];
 logx = log(x);
@@ -166,35 +161,27 @@ p = length(x);
 
 % ----------------------------------------------------------------------
 % Initialise regularisation
-if isfield(prm,'regul')
-    LPRM.hReg = toastRegul (prm.regul, LPRM.hBasis, logx);
-else
-    LPRM.hReg = 0;
-end
+%LPRM.hReg = 0;
+%if isfield(prm.regul,'method') && ~strcmpi(prm.regul.method,'none')
+%    LPRM.hReg = toastRegul (prm.regul, logx);
+%end
 
 % ----------------------------------------------------------------------
 % Inverse solver loop
 switch prm.solver.method
     case 'LM'
-        toastSolverGN (prm, LPRM, ref, x);
+        solver = toastSolverGN (prm);
+        solver.Solve (x);
     case 'GN_implicit'
         toastSolverGNimplicit (prm, LPRM, ref, x);
     case 'PCG'
-        toastSolverCG (prm, LPRM, ref, x);
+        solver = toastSolverCG (prm);
+        solver.Solve (x);
     case 'LBFGS'
-        toastSolverLBFGS (prm, LPRM, ref, x);
+        solver = toastSolverLBFGS (prm);
+        solver.Solve (x);
 end
         
-% ----------------------------------------------------------------------
-% Cleanup
-toastClearRegul(LPRM.hReg);
-if isfield(prm.solver.basis,'hbasis') == false
-    toastDeleteBasis(LPRM.hBasis);
-end
-if isfield(prm.fwdsolver,'hmesh') == false
-    toastDeleteMesh(LPRM.hMesh);
-end
-
 
 end % of toastRecon
 
@@ -203,8 +190,17 @@ end % of toastRecon
 % =============================================================
 function [prm,localprm] = checkprm(prm)
 
+if ~isa(prm,'toastParam')
+    error('Expected a toastParam argument.');
+end
+
+% check for mandatory fields
+if isfield(prm.solver,'basis') == false
+    error('Required parameter field "solver.basis" is missing.');
+end
+
 % fill missing parameter entries
-if isfield(prm,'solver') == false || isfield(prm.solver,'method') == false || ischar(prm.solver.method) == false || length(prm.solver.method) == 0
+if isfield(prm.solver,'method') == false || ischar(prm.solver.method) == false || length(prm.solver.method) == 0
     prm.solver.method = 'PCG';
 end
 if isfield(prm.solver,'tol') == false || isnumeric(prm.solver.tol) == false
@@ -279,17 +275,17 @@ end
 
 % =============================================================
 function prm=resetprm(cfg,hmesh)
-n = toastMeshNodeCount (hmesh);
+n = hmesh.NodeCount();
 switch upper(cfg.reset)
     case 'HOMOG'
         prm = ones(n,1) * cfg.val;
     case 'NIM'
-        prm = toastReadNIM(cfg.nim);
+        prm = toastNim(cfg.nim);
         if length(prm) ~= n
             disp('Warning: incompatible size of NIM file')
         end
     case 'NIM_LAST'
-        prm = toastReadNIM(cfg.nim,0);
+        prm = toastNim(cfg.nim,0);
         if length(prm) ~= n
             disp('Warning: incompatible size of NIM file')
         end
@@ -315,18 +311,18 @@ sckap = x(ps+1:2*ps);
 smua  = scmua./scref;
 skap  = sckap./scref;
 smus  = 1./(3*skap) - smua;
-RES.bmua = zeros(prod(RES.bdim),1);  RES.bmua(RES.solmask) = smua;
-RES.bmus = zeros(prod(RES.bdim),1);  RES.bmus(RES.solmask) = smus;
+RES.bmua = LPRM.hBasis.Map ('S->B', smua);
+RES.bmus = LPRM.hBasis.Map ('S->B', smus);
 RES.of(itr+1) = err;
 
 if isfield(prm,'callback') && isfield(prm.callback,'iter')
     % let the calling function do the display
     if isfield(prm.callback,'request') % check requests for secondary results
         if isfield(prm.callback.request,'prior') && prm.callback.request.prior == true
-            RES.kapref = toastRegulKappa (LPRM.hReg,log(x));
+            RES.kapref = LPRM.hReg.Kappa (log(x));
         end
     end
-    feval(prm.callback.iter, prm.callback.context, RES);
+    feval(prm.callback.iter, prm, RES);
 else
     % inline display
     disp_iter(RES);
